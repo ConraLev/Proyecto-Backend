637154486591a8cafd17652c86ca4a96b8c880ee
ConraLev <conradolevanti@gmail.com>
2024-06-17 18:20:22 -0300

    Implementacion de mock

--- src/routers/products.Router.js
+++ src/routers/products.Router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const generateMockProducts = require('../mocks/products.mocking');
 
 const configure = (app) => {
     const productController = app.get('productController');
@@ -7,12 +8,19 @@ const configure = (app) => {
     if (!productController) {
         throw new Error('ProductController is not defined');
     }
+    
+    router.get('/mockingproducts', (req, res) => {
+        const products = generateMockProducts();
+        res.json(products);
+    });
 
     router.get('/', productController.getAll.bind(productController));
     router.get('/:id', productController.getById.bind(productController));
     router.post('/', productController.createOne.bind(productController));
     router.put('/:id', productController.updateOne.bind(productController));
     router.delete('/:id', productController.deleteById.bind(productController));
+
+
 
     app.use('/products', router);
 };
