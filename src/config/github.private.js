c1a19acccd725676cbafd84d4df9dee87a372478
ConraLev <conradolevanti@gmail.com>
2024-08-14 12:56:44 -0300

    Correccion de direccion

--- src/config/github.private.js
+++ src/config/github.private.js
@@ -13,6 +13,6 @@ module.exports = {
     clientID: 'Iv1.e9bc8072805cd4e7',
     clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
     callbackURL: isProduction 
-        ? 'https://proyecto-backend-production-c56e.up.railway.app/sessions/githubcallback'
+        ? 'https://https://proyecto-ecommerce.up.railway.app/sessions/githubcallback'
         : `http://localhost:${process.env.PORT}/sessions/githubcallback`
 };
