727b8e3e8c6aadf2e3f2f8b0c1cf96407f362fe1
ConraLev <conradolevanti@gmail.com>
2024-08-14 19:02:35 -0300

    Correcion de direccion

--- src/config/github.private.js
+++ src/config/github.private.js
@@ -1,12 +1,8 @@
-// module.exports = {
-//     appId: '886871',
-//     clientID: 'Iv1.e9bc8072805cd4e7',
-//     clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
-//     callbackURL: process.env.CALLBACKURL};
+
+module.exports = {
+    appId: '886871',
+    clientID: 'Iv1.e9bc8072805cd4e7',
+    clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
+    callbackURL: process.env.CALLBACKURL};
 
 
-    module.exports = {
-        appId: '886871',
-        clientID: 'Iv1.e9bc8072805cd4e7',
-        clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
-        callbackURL: `${process.env.CALLBACKURL}/sessions/githubcallback`};
