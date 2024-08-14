0e2fde921743d1e7663a6ccc5afd32aa3528e311
ConraLev <conradolevanti@gmail.com>
2024-08-14 17:11:35 -0300

    Correcion de direccion

--- src/config/github.private.js
+++ src/config/github.private.js
@@ -1,25 +1,25 @@
-// const isProduction = process.env.ENV === 'prod';
-
-// module.exports = {
-//     appId: '886871',
-//     clientID: 'Iv1.e9bc8072805cd4e7',
-//     clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
-//     callbackURL: isProduction 
-//         ? 'https://proyecto-ecommerce.up.railway.app/sessions/githubcallback'
-//         : `http://localhost:${process.env.PORT}/sessions/githubcallback`
-// };
-
-
 const isProduction = process.env.ENV === 'prod';
-
-const callbackURL = isProduction 
-    ? 'https://proyecto-ecommerce.up.railway.app/sessions/githubcallback'
-    : `http://localhost:${process.env.PORT}/sessions/githubcallback`;
 
 module.exports = {
     appId: '886871',
     clientID: 'Iv1.e9bc8072805cd4e7',
     clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
-    callbackURL
+    callbackURL: isProduction 
+        ? 'https://proyecto-ecommerce.up.railway.app/sessions/githubcallback'
+        : `http://localhost:${process.env.PORT}/sessions/githubcallback`
 };
+
+
+// const isProduction = process.env.ENV === 'prod';
+
+// const callbackURL = isProduction 
+//     ? 'https://proyecto-ecommerce.up.railway.app/sessions/githubcallback'
+//     : `http://localhost:${process.env.PORT}/sessions/githubcallback`;
+
+// module.exports = {
+//     appId: '886871',
+//     clientID: 'Iv1.e9bc8072805cd4e7',
+//     clientSecret: '868670bb545e8b6b8f8716af8abd2be62513771b',
+//     callbackURL
+// };
 
